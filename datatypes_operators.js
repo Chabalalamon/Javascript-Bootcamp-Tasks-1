3d04de4a94225e941750a723b8628bf82d0b8ebd
Chabalalamon <152514951+Chabalalamon@users.noreply.github.com>
2026-05-27 00:43:08 +0200

    Add datatypes and operators exercises in JS

---
+++ datatypes_operators.js
@@ -0,0 +1,41 @@
+// Exercise 1: Numbers
+let integerNumber = 10;
+let floatingNumber = 3.14;
+
+console.log(integerNumber + 5);
+console.log(integerNumber - 5);
+console.log(integerNumber * 5);
+console.log(integerNumber / 5);
+console.log(integerNumber % 3);
+console.log(integerNumber ** 2);
+
+console.log(integerNumber);
+console.log(floatingNumber);
+
+// Exercise 2: Boolean and Operators
+let x = 8;
+let y = 12;
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x === y);
+console.log(x !== y);
+
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let p = 10;
+p += 5;
+console.log(p);
+p -= 3;
+console.log(p);
+p *= 2;
+console.log(p);
+p /= 4;
+console.log(p);
+p %= 3;
+console.log(p);
