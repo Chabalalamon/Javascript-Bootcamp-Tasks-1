e6f15dcbbb7efe6ce24df74bcf2da2dc975f89ac
Chabalalamon <152514951+Chabalalamon@users.noreply.github.com>
2026-05-27 00:42:00 +0200

    Add variables.js with exercises on variables

---
+++ variables.js
@@ -0,0 +1,29 @@
+// Exercise 1: Variable Declaration
+let name = "Monica";
+let age = 27;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// Exercise 2: Variable Reassignment
+let favouriteColour = "orange";
+console.log(favouriteColour);
+
+favouriteColour = "cobalt blue";
+console.log(favouriteColour);
+
+// Exercise 3: Arithmetic Operations
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
